a690dc129506242a6a98bd68b204466ac5756c52
aleksey <mitakacutie@gmail.com>
2026-03-08 12:25:08 +0300

    feat: setup trpc client

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,21 @@
+import { trpc } from './api/trpc';
+
 export default function App() {
+  const query = trpc.flights.getAll.useQuery({
+    country: 'Russian Federation',
+  });
+
+  if (query.isPending) {
+    return (
+      <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
   return (
     <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
-      <h1 className="text-2xl">✈️ Flight Tracker</h1>
+      <h1 className="text-2xl">✈️ Flights: {query.data?.total}</h1>
     </div>
   );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,18 @@
+import './index.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { trpc, trpcClient } from './api/trpc';
 import App from './App';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </trpc.Provider>
   </StrictMode>,
 );
