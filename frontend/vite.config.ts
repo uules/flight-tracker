c010e7a8040342ffbffee614ec34c9ec7f216307
aleksey <mitakacutie@gmail.com>
2026-03-08 12:12:18 +0300

    init: frontend vite

---
+++ frontend/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@backend': path.resolve(__dirname, '../backend/src'),
+    },
+  },
+  server: {
+    port: 5173,
+    proxy: {
+      '/trpc': 'http://localhost:3000',
+    },
+  },
+});
