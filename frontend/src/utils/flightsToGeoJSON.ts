947909ab52de2db3e6245b5cede6c04e29149339
aleksey <mitakacutie@gmail.com>
2026-03-09 11:02:44 +0300

    feat: flightsToGeoJSON util

---
+++ frontend/src/utils/flightsToGeoJSON.ts
@@ -0,0 +1,24 @@
+import { Flight } from '@backend/types/flights';
+import type { FeatureCollection, Point } from 'geojson';
+
+export function flightsToGeoJSON(flights: Flight[]): FeatureCollection<Point> {
+  return {
+    type: 'FeatureCollection',
+    features: flights.map((flight) => ({
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [flight.longitude, flight.latitude],
+      },
+      properties: {
+        icao24: flight.icao24,
+        callsign: flight.callsign,
+        country: flight.country,
+        altitude: flight.altitude,
+        velocity: flight.velocity,
+        heading: flight.heading ?? 0,
+        onGround: flight.onGround,
+      },
+    })),
+  };
+}
